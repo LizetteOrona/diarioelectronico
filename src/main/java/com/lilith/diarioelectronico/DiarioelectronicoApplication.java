e06a7f3444310d5bac4e8fd19296050fbe377d4b
Lizette Orona <lizettejimenezorona@gmail.com>
2019-03-22 00:19:45 -0600

    Initial commit

---
+++ src/main/java/com/lilith/diarioelectronico/DiarioelectronicoApplication.java
@@ -0,0 +1,13 @@
+package com.lilith.diarioelectronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioelectronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioelectronicoApplication.class, args);
+	}
+
+}
